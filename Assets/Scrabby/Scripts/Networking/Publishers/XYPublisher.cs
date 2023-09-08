dee646ef8bf284e5f3e2fcb83023ec065bacd974
Dylan Zemlin <dylan.zemlin@ou.edu>
2023-09-08 11:38:14 -0500

    Moved publishers and added new position publisher

---
+++ Assets/Scrabby/Scripts/Networking/Publishers/XYPublisher.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using Scrabby.ScriptableObjects;
+using UnityEngine;
+
+namespace Scrabby.Networking.Publishers
+{
+    public class XYPublisher : MonoBehaviour
+    {
+        private JObject _xyData;
+        private float _nextPublishTime;
+
+        private void Start()
+        {
+            _xyData = new JObject();
+        }
+
+        private void FixedUpdate()
+        {
+            if (Time.time < _nextPublishTime) return;
+
+            _nextPublishTime = Time.time + 1.0f / 2.0f;
+            var t = transform.position;
+            _xyData["x"] = t.x;
+            _xyData["y"] = t.z;
+            Network.instance.Publish("/onboarding/position", "geometry_msgs/Point", _xyData);
+        }
+    }
+}
